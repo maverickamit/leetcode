5e00e7512da8cb06b73727f895d8fb26249af63b
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2023-03-26 22:56:31 +0530

    (Update) Prob 1: Two sum with O(n) time complexity

--- prob1.ts
+++ prob1.ts
@@ -1,10 +1,27 @@
+interface Nums {
+  [key: number]: number; // value of number : index of number
+}
+
 function twoSum(nums: number[], target: number): number[] {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (nums[i] + nums[j] == target && j !== i) {
-        return [i, j];
-      }
+  let length = nums.length;
+  let ans: number[] = [];
+
+  //Creating a nums object with the number as key and index as value
+  let numsObj: Nums = {};
+  for (let i = 0; i < length; i++) {
+    numsObj[nums[i]] = i;
+  }
+  //Checking if difference value exists in the nums object and if it does
+  //getting the index of it.
+  for (let i = 0; i < length; i++) {
+    let val = target - nums[i];
+    let index = numsObj[val];
+    if (index && index !== i) {
+      //Making sure we get two unique indexes
+      ans = [i, index];
+      break;
     }
   }
-  return [];
+
+  return ans;
 }
