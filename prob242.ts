f39a64a8fdca68c709890c8ed6b7b568a6a9ba56
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2023-03-26 23:00:04 +0530

    (Update) Prob 242: Valid anagram with O(n) time complexity

--- prob242.ts
+++ prob242.ts
@@ -1,19 +1,30 @@
-function isAnagram(s: string, t: string): boolean {
-  let length1 = s.length;
-  let length2 = t.length;
-  let ans = true;
-  if (length1 !== length2) return false;
+interface Characters {
+  [charIndex: number]: number; //charIndex starts at 0 for a to 25 for z
+}
 
-  let i = 0;
-  while (length1 - i > 0) {
-    if (s.includes(t[0])) {
-      s = s.replace(t[0], "");
-      t = t.replace(t[0], "");
-      i++;
-    } else {
-      ans = false;
-      break;
+function isAnagram(s: string, t: string): boolean {
+  let charactersObj: Characters = {};
+  for (let i = 0; i < 26; i++) {
+    charactersObj[i] = 0;
+  }
+  if (s.length) {
+    //Only count characters if the string length is greater than 0
+    for (let i = 0; i < s.length; i++) {
+      charactersObj[s[i].charCodeAt(0) - 97]++;
     }
   }
+  if (t.length) {
+    //Only count characters if the string length is greater than 0
+    for (let i = 0; i < t.length; i++) {
+      charactersObj[t[i].charCodeAt(0) - 97]--;
+    }
+  }
+  let ans = true;
+  Object.values(charactersObj).forEach((value) => {
+    if (value !== 0) {
+      ans = false;
+    }
+  });
+
   return ans;
 }
