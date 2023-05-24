2977ca1de09fe2b6149db79a65dd37464eaa89c3
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2023-05-24 09:58:12 +0530

    (Update) Prob 1: Two Sum - Solve using Map - Iterate over the array only once

--- prob1.ts
+++ prob1.ts
@@ -1,27 +1,15 @@
-interface Nums {
-  [key: number]: number; // value of number : index of number
-}
-
 function twoSum(nums: number[], target: number): number[] {
-  let length = nums.length;
-  let ans: number[] = [];
-
-  //Creating a nums object with the number as key and index as value
-  let numsObj: Nums = {};
-  for (let i = 0; i < length; i++) {
-    numsObj[nums[i]] = i;
-  }
-  //Checking if difference value exists in the nums object and if it does
-  //getting the index of it.
-  for (let i = 0; i < length; i++) {
-    let val = target - nums[i];
-    let index = numsObj[val];
-    if (index && index !== i) {
-      //Making sure we get two unique indexes
-      ans = [i, index];
-      break;
+  //Creating a nums map with the number as key and index as value
+  let numsMap = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    //Checking if difference value exists in the nums map and if it does
+    //getting the index of it.
+    let diff = target - nums[i];
+    if (numsMap.has(diff)) {
+      let indexOfDiff = numsMap.get(diff);
+      if (indexOfDiff !== undefined) return [i, indexOfDiff];
     }
+    numsMap.set(nums[i], i);
   }
-
-  return ans;
+  return [];
 }
