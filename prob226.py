aef79dec6a4198814727da3571690c9ce596a271
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2025-02-03 06:54:50 +0000

    Prob 226: Invert Binary Tree

---
+++ prob226.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def explore(node):
+            if not node:
+                return None
+            right = node.right if node.right else None
+            left = node.left if node.left else None
+            node.right = left
+            node.left = right
+            explore(node.right)
+            explore(node.left)
+
+
+        explore(root)
+
+        return root
